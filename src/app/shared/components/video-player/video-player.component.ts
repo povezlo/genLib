47ca1e8a66ce41816207671cc69bbeeaeebda781
Andrey Rybak <aliester.crowly@gmail.com>
2023-03-18 17:39:37 +0200

    set save progress video"

--- src/app/shared/components/video-player/video-player.component.ts
+++ src/app/shared/components/video-player/video-player.component.ts
@@ -4,13 +4,13 @@ import {
   ElementRef,
   Input,
   ViewChild,
-  OnDestroy
+  OnDestroy,
+  Renderer2
 } from '@angular/core';
+import { MatTooltip } from '@angular/material/tooltip';
 import Hls from 'hls.js';
 
 const HLS_MEDIA_TYPE = 'application/vnd.apple.mpegurl';
-const ID_VIDEO = 'id_video';
-
 
 @Component({
   selector: 'app-video-player',
@@ -18,15 +18,21 @@ const ID_VIDEO = 'id_video';
   styleUrls: ['./video-player.component.scss']
 })
 export class VideoPlayerComponent implements AfterViewInit, OnDestroy {
+  @Input() id = '';
   @Input() url = '';
   @Input() poster?: string;
   @Input() title?: string;
-  @ViewChild('videoPlayer') videoElementRef!: ElementRef;
+  @ViewChild('videoPlayer') videoElementRef!: ElementRef; 
+  @ViewChild(MatTooltip) tooltip!: MatTooltip;
 
   videoPlayer!: HTMLVideoElement;
-  private defaultPlaybackRate = 0;
-  private hls = new Hls();
 
+  private hls = new Hls();
+  private defaultPlaybackRate = 0;
+  
+  constructor(private renderer: Renderer2) { }
+
+  
   ngAfterViewInit(): void {
     this.initVideoElement();
   }
@@ -35,10 +41,8 @@ export class VideoPlayerComponent implements AfterViewInit, OnDestroy {
     this.videoPlayer = this.videoElementRef?.nativeElement;
     
     if(this.poster)  this.videoPlayer.poster = this.poster;
-
     this.defaultPlaybackRate = this.videoPlayer.defaultPlaybackRate;
- 
-    this.videoPlayer.play();
+    this.videoPlayer.currentTime = this.getProgress();
 
     if (Hls.isSupported()) {
       console.log("Video streaming supported by HLSjs", this.videoPlayer.currentTime)
@@ -46,12 +50,9 @@ export class VideoPlayerComponent implements AfterViewInit, OnDestroy {
 
       this.hls.loadSource(this.url);
       this.hls.attachMedia(this.videoPlayer);
+      this.saveProgress();
 
-      this.setStartTime()
-      this.videoPlayer.addEventListener('timeOut', (e)=> console.log(e))
-      this.hls.on(Hls.Events.LEVEL_LOADED, (e, data) => {
-        console.log('LEVEL_LOADED', data);
-      });
+      this.videoPlayer.addEventListener('pause', () => this.saveProgress());
     }
 
     else if (this.videoPlayer.canPlayType(HLS_MEDIA_TYPE)) {
@@ -59,28 +60,33 @@ export class VideoPlayerComponent implements AfterViewInit, OnDestroy {
     }
   }
 
-  setStartTime(): void {
-    const currentTime = localStorage.getItem(ID_VIDEO) || 0;
-    if(currentTime && typeof currentTime === 'number') {
-      this.videoPlayer.currentTime = currentTime;
-    }
+  getProgress(): number {
+    return Number(localStorage.getItem(this.id)) || 0;
   }
 
-  onKeyDown(event: KeyboardEvent) {
+  saveProgress(): void {
+    localStorage.setItem(this.id, String(this.videoPlayer.currentTime));
+  }
+
+  setPlaybackRate(event: KeyboardEvent): void {
     switch (event.key) {
       case '0':
         this.videoPlayer.playbackRate = this.defaultPlaybackRate;
+        this.tooltip.show();
         break;
       case '2':
         if (event.shiftKey) {
           this.videoPlayer.playbackRate = 2;
+          this.tooltip.show();
         }
         break;
       case '+':
         this.videoPlayer.playbackRate += 0.25;
+          this.tooltip.show();
         break;
       case '-':
         this.videoPlayer.playbackRate -= 0.25;
+          this.tooltip.show();
         break;
     }
   }
--- src/app/shared/components/video-player/video-player.module.ts
+++ src/app/shared/components/video-player/video-player.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VideoPlayerComponent } from './video-player.component';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 
 
 @NgModule({
   declarations: [VideoPlayerComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatTooltipModule
   ],
   exports: [VideoPlayerComponent]
 })
