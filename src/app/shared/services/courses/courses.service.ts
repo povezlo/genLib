fbd8e84b5744f7d19ee5ebe5dce9efd15d9f8e2a
Andrey Rybak <aliester.crowly@gmail.com>
2023-03-20 00:53:08 +0200

    fixes bugs"

--- src/app/shared/services/courses/courses.service.ts
+++ src/app/shared/services/courses/courses.service.ts
@@ -1,27 +1,18 @@
 import { ApiClientBaseService } from '../api/api-client-base.service';
 import { Injectable } from '@angular/core';
-
 import { Observable } from 'rxjs';
-import { shareReplay } from 'rxjs/operators';
-
 import { ICoursesResponse, ICoursePreviewResponse } from '../../interfaces'
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
-  private courses$: Observable<ICoursesResponse> | null = null;
 
   constructor(private apiService: ApiClientBaseService) {}
 
 
   getCourses(): Observable<ICoursesResponse> | null {
-      if (!this.courses$) {
-          this.courses$ = this.apiService.get<ICoursesResponse>('core/preview-courses')
-          .pipe(shareReplay());
-    }
-
-    return this.courses$;
+      return this.apiService.get<ICoursesResponse>('core/preview-courses');
   }
 
 
